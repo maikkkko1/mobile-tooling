2dabcd89a6971f70372d7690c4ea1911ca2edf3b
Maikon Ferreira <mai.kon96@hotmail.com>
2020-12-01 15:03:24 -0300

    script exec

--- run.js
+++ run.js
@@ -1,16 +1,7 @@
 var exec = require("child_process").exec;
 
-exec("npm i --no-warnings", function (err, out, stderr) {
-  const cmd = require("node-cmd");
+console.log("########### Iniciando Unicred Mobile Tooling... ###########");
 
-  console.log("Iniciando Unicred Mobile Tooling...");
-
-  cmd.runSync("cd app && npm i --no-warnings && npm i --save-dev sass");
-  cmd.runSync("cd server && npm i --no-warnings && npm i sqlite3 --save");
-
-  cmd.runSync("cd server && npx sequelize-cli db:migrate");
-
-  cmd.runSync("npm start");
-
-  console.log("Iniciando Unicred Mobile Tooling...");
+exec("npm start", function (err, out, stderr) {
+  console.log("########### Unicred Mobile Tooling Inicializado! ###########");
 });
